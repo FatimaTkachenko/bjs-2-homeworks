e1aa795465cf33e6b6c9f92f6579aca6d5343b57
Fatima <fat.tka4enko2014@yandex.ru>
2026-03-15 18:08:49 +0300

    адача 1: квадратные уравнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+  const arr = [];
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) return arr;
+
+  if (d === 0) {
+    const x = -b / (2 * a);
+    arr.push(x);
+    return arr;
+  }
+
+  const x1 = (-b + Math.sqrt(d)) / (2 * a);
+  const x2 = (-b - Math.sqrt(d)) / (2 * a);
+  arr.push(x1, x2);
+  return arr;
 }
