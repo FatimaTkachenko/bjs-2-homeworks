71a37c9d44364f55deb549e8a8c2d0a41c564054
Fatima <fat.tka4enko2014@yandex.ru>
2026-03-28 16:08:21 +0300

    обавлено решение заданий 1 и 2 в task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,22 +1,34 @@
+"use strict";
+// Задание 1: Решение квадратного уравнения
+function solveEquation(a, b, c) {
+    const discriminant = b ** 2 - 4 * a * c;
+    if (discriminant < 0) {
+        return [];
+    } else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        return [root];
+    } else {
+        const sqrtD = Math.sqrt(discriminant);
+        const root1 = (-b + sqrtD) / (2 * a);
+        const root2 = (-b - sqrtD) / (2 * a);
+        return [root1, root2];
+    }
+}
+// Задание 2: Калькулятор ипотеки
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-    const params = [percent, contribution, amount, countMonths];
-
-    for (let param of params) {
-        const num = Number(param);
-        if (Number.isNaN(num)) {
-            return false;
-        }
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
+    countMonths = Number(countMonths);
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+        return false;
     }
-
-    const p = percent / 100 / 12;
-    const s = amount - contribution;
-
-    if (s <= 0) {
-        return Number(0.toFixed(2));
+    const principal = amount - contribution;
+    if (principal <= 0) {
+        return 0;
     }
-
-    const payment = s * (p + (p / (Math.pow((1 + p), countMonths) - 1)));
-    const totalPayment = payment * countMonths;
-
+    const monthlyRate = (percent / 100) / 12;
+    const monthlyPayment = principal * (monthlyRate + (monthlyRate / ((1 + monthlyRate) ** countMonths - 1)));
+    const totalPayment = monthlyPayment * countMonths + contribution;
     return Number(totalPayment.toFixed(2));
 }
